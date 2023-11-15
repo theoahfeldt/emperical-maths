cc04d74abf0b9ee1f9742e2ec136bed9a0f6c891
Theo Puranen Åhfeldt <34159999+theoahfeldt@users.noreply.github.com>
2023-11-15 14:08:31 +0100

    Add underscore in front of private function

--- game/sum.gd
+++ game/sum.gd
@@ -6,7 +6,7 @@ class_name Sum
 @export var b: AlgebraicExpression
 
 
-func get_components():
+func _get_components() -> Array:
 	return [
 		$LeftParenthesis,
 		a,
@@ -18,24 +18,24 @@ func get_components():
 
 func get_width() -> int:
 	var width = 0
-	for component in get_components():
+	for component in _get_components():
 		width += component.get_width()
 	return width
 
 
-	for component in get_components():
 func set_color(color: Color) -> void:
+	for component in _get_components():
 		component.set_color(color)
 
 
-	for component in get_components():
 func set_color_by_depth(depth: int) -> void:
+	for component in _get_components():
 		component.set_color_by_depth(depth + 1)
 
 
 func set_term_positions() -> void:
 	var width = 0
-	for component in get_components():
+	for component in _get_components():
 		component.position = Vector2(width, 0)
 		width += component.get_width()
 
