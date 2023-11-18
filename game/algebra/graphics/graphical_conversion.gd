1a4db90e5063e8631e2838b5e2533ae70055ab97
Theo Puranen Åhfeldt <34159999+theoahfeldt@users.noreply.github.com>
2023-11-18 17:25:44 +0100

    Refactor code

--- game/algebra/graphics/graphical_conversion.gd
+++ game/algebra/graphics/graphical_conversion.gd
@@ -12,14 +12,9 @@ static func convert_variable(variable: AlgebraicVariable) -> GraphicalExpression
 
 
 static func convert_sum(sum: AlgebraicSum) -> GraphicalExpression:
-	var new: GraphicalSum = sum_scene.instantiate()
 	var left = algebraic_to_graphical(sum.left_term)
 	var right = algebraic_to_graphical(sum.right_term)
-	new.add_child(left)
-	new.add_child(right)
-	new.left_term = left
-	new.right_term = right
-	return new
+	return _create_sum(left, right)
 
 
 static func algebraic_to_graphical(
@@ -37,22 +32,15 @@ static func convert_variable_menu(
 		variable: AlgebraicVariable, rules: Array) -> GraphicalExpression:
 	if variable.is_selected:
 		return ExpressionsMenu.from_expression(variable, rules)
-	var new: GraphicalVariable = variable_scene.instantiate()
-	new.variable_name = variable.variable_name
-	return new
+	return convert_variable(variable)
 
 
 static func convert_sum_menu(sum: AlgebraicSum, rules: Array) -> GraphicalExpression:
 	if sum.is_selected:
 		return ExpressionsMenu.from_expression(sum, rules)
-	var new: GraphicalSum = sum_scene.instantiate()
 	var left = algebraic_to_graphical_menu(sum.left_term, rules)
 	var right = algebraic_to_graphical_menu(sum.right_term, rules)
-	new.add_child(left)
-	new.add_child(right)
-	new.left_term = left
-	new.right_term = right
-	return new
+	return _create_sum(left, right)
 
 
 static func algebraic_to_graphical_menu(
@@ -64,3 +52,13 @@ static func algebraic_to_graphical_menu(
 	else:
 		push_error("Conversion not implemented for %s" % expression.get_class())
 		return GraphicalExpression.new()
+
+
+static func _create_sum(
+		left: GraphicalExpression, right: GraphicalExpression) -> GraphicalSum:
+	var sum: GraphicalSum = sum_scene.instantiate()
+	sum.add_child(left)
+	sum.add_child(right)
+	sum.left_term = left
+	sum.right_term = right
+	return sum
