42b815ad74b792e2f61221c61b217d57e8c46837
Theo Puranen Åhfeldt <34159999+theoahfeldt@users.noreply.github.com>
2023-11-18 16:06:46 +0100

    Add rule for backwards direction of associtivity for sums

--- game/algebra/logic/rules/rules.gd
+++ game/algebra/logic/rules/rules.gd
@@ -1,11 +1,13 @@
 const Identity = preload("res://algebra/logic/rules/identity.gd")
-const CommutativitySum = preload("res://algebra/logic/rules/commutativity_sum.gd")
 const AssociativityForwardSum = preload("res://algebra/logic/rules/associaticity_forward_sum.gd")
+const AssociativityBackwardSum = preload("res://algebra/logic/rules/associaticity_backward_sum.gd")
+const CommutativitySum = preload("res://algebra/logic/rules/commutativity_sum.gd")
 
 
 static func rules() -> Array:
 	return [
 		Identity.new(),
-		CommutativitySum.new(),
 		AssociativityForwardSum.new(),
+		AssociativityBackwardSum.new(),
+		CommutativitySum.new(),
 	]
