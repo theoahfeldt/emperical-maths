5505fa3de2ad57ae1a48988170d36c79af447098
Theo Puranen Åhfeldt <34159999+theoahfeldt@users.noreply.github.com>
2023-11-19 21:13:28 +0100

    Create own folder for sum rules

--- game/algebra/logic/rules/rules.gd
+++ game/algebra/logic/rules/rules.gd
@@ -1,11 +1,11 @@
 const Identity = preload("res://algebra/logic/rules/identity.gd")
-const SumAssociativityForward = preload("res://algebra/logic/rules/sum_associaticity_forward.gd")
-const SumAssociativityBackward = preload("res://algebra/logic/rules/sum_associaticity_backward.gd")
-const SumCommutativity = preload("res://algebra/logic/rules/sum_commutativity.gd")
-const SumIdentityLeftForward = preload("res://algebra/logic/rules/sum_identity_left_forward.gd")
-const SumIdentityLeftBackward = preload("res://algebra/logic/rules/sum_identity_left_backward.gd")
-const SumIdentityRightForward = preload("res://algebra/logic/rules/sum_identity_right_forward.gd")
-const SumIdentityRightBackward = preload("res://algebra/logic/rules/sum_identity_right_backward.gd")
+const SumAssociativityForward = preload("res://algebra/logic/rules/sum/associaticity_forward.gd")
+const SumAssociativityBackward = preload("res://algebra/logic/rules/sum/associaticity_backward.gd")
+const SumCommutativity = preload("res://algebra/logic/rules/sum/commutativity.gd")
+const SumIdentityLeftForward = preload("res://algebra/logic/rules/sum/identity_left_forward.gd")
+const SumIdentityLeftBackward = preload("res://algebra/logic/rules/sum/identity_left_backward.gd")
+const SumIdentityRightForward = preload("res://algebra/logic/rules/sum/identity_right_forward.gd")
+const SumIdentityRightBackward = preload("res://algebra/logic/rules/sum/identity_right_backward.gd")
 
 
 static func rules() -> Array:
