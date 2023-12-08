27087e0ec9a6a1de96c51e4dbb37051ff0c1400a
Theo Puranen Åhfeldt <34159999+theoahfeldt@users.noreply.github.com>
2023-12-08 10:19:55 +0100

    Update error messages of algebraic expression's abstract functions
    To be consistent with the rest of the project.

--- game/algebra/logic/expressions/algebraic_expression.gd
+++ game/algebra/logic/expressions/algebraic_expression.gd
@@ -15,17 +15,17 @@ var color: Color
 
 
 func identical_to(_other: AlgebraicExpression) -> bool:
-	push_error("Method not implemented.")
+	push_error("Function not implemented")
 	return false
 
 
 func copy() -> AlgebraicExpression:
-	push_error("Method not implemented.")
+	push_error("Function not implemented")
 	return AlgebraicExpression.new()
 
 
 func pretty_string() -> String:
-	push_error("Method not implemented.")
+	push_error("Function not implemented")
 	return ""
 
 
