2473bfa407b1a553ae82ec9895cb9dfe099c66e6
Theo Puranen Åhfeldt <34159999+theoahfeldt@users.noreply.github.com>
2023-12-13 15:41:05 +0100

    Add two levels

---
+++ game/levels/level2.gd
@@ -0,0 +1,14 @@
+extends LevelCreator
+
+
+static func create() -> Level:
+	var left := AlgebraicSum.create(
+			AlgebraicInteger.zero(), AlgebraicVariable.create("a"))
+	var right := AlgebraicVariable.create("a")
+	var equality := AlgebraicEquality.create(left, right)
+	var algebraic_rules: Array[AlgebraicRule] = [
+		Identity.new(),
+		SumIdentityLeftForward.new(),
+		SumIdentityLeftBackward.new(),
+	]
+	return Level.create(equality, algebraic_rules, [])
