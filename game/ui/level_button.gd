c2390953abd31a687f2a745645b3ded3c75e50c2
Theo Puranen Åhfeldt <34159999+theoahfeldt@users.noreply.github.com>
2024-01-05 20:10:57 +0100

    Name class LevelButton

--- game/ui/level_button.gd
+++ game/ui/level_button.gd
@@ -1,3 +1,4 @@
+class_name LevelButton
 extends Button
 
 
--- game/ui/level_select_menu.gd
+++ game/ui/level_select_menu.gd
@@ -6,7 +6,7 @@ signal selected(assertion: Resource)
 
 
 func _ready() -> void:
-	for button in get_tree().get_nodes_in_group("level_buttons"):
+	for button: LevelButton in get_tree().get_nodes_in_group("level_buttons"):
 		button.pressed.connect(
 				func(): _on_level_button_pressed(button.assertion))
 
