fa431e0beadc58b1c6f83319038c49511981088c
Theo Puranen Åhfeldt <34159999+theoahfeldt@users.noreply.github.com>
2024-01-11 14:08:14 +0100

    Add code to automatically set text and dependencies for LevelButton

--- game/ui/level_button.gd
+++ game/ui/level_button.gd
@@ -1,6 +1,19 @@
+@tool
 class_name LevelButton
 extends Button
 
 
-@export var assertion: Assertion
+@export var assertion: Assertion:
+	set(value):
+		assertion = value
+		text = value.equality
+		_find_dependencies()
 @export var dependencies: Array[LevelButton]
+
+
+func _find_dependencies() -> void:
+	dependencies = []
+	if assertion is Consequence:
+		for button: LevelButton in get_tree().get_nodes_in_group("level_buttons"):
+			if button.assertion.equality in assertion.premises.map(func(a): return a.equality):
+				dependencies.append(button)
