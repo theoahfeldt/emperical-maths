a3cfb92ebe5ea1b27c7ed53717f2ed5a617620ff
Theo Puranen Åhfeldt <34159999+theoahfeldt@users.noreply.github.com>
2024-01-11 20:45:31 +0100

    Delete deprecated tests

--- game/algebra/ui/tests/test_expression_controller.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	var level: Level = load("res://levels/level1.gd").create()
-	var expression_controller := ExpressionController.create(
-			level.equality, level.algebraic_rules, level.substitution_rules)
-	add_child(expression_controller)
