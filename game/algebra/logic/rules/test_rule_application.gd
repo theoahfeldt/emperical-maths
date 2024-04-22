0f2f89651c2c1715fc344adde21dfc796fc2ece5
Theo Puranen Åhfeldt <34159999+theoahfeldt@users.noreply.github.com>
2024-04-22 19:10:19 +0200

    Fix test script for rule application

--- game/algebra/logic/rules/test_rule_application.gd
+++ game/algebra/logic/rules/test_rule_application.gd
@@ -2,7 +2,8 @@ extends Node
 
 
 func _ready() -> void:
-	var rule := ManipulationRule.parse("(a+0)", "a")
+	var rule := AlgebraicRule.create(AlgebraicExpression.from_string("(a+0)"),
+			AlgebraicExpression.from_string("a"))
 	var expression := AlgebraicExpression.from_string("(b+0)")
 	var result := rule.apply(expression)
 	assert(result is ApplicationSuccess)
@@ -10,7 +11,8 @@ func _ready() -> void:
 	var expression2 := AlgebraicExpression.from_string("(0+b)")
 	var result2 := rule.apply(expression2)
 	assert(result2 is ApplicationFailure)
-	var rule2 := ManipulationRule.parse("0", "(a+-a)")
+	var rule2 := AlgebraicRule.create(AlgebraicExpression.from_string("0"),
+			AlgebraicExpression.from_string("(a+-a)"))
 	var expression3 := AlgebraicExpression.from_string("0")
 	var result3 := rule2.apply(expression3)
 	assert(result3 is ApplicationSuccess)
@@ -19,7 +21,7 @@ func _ready() -> void:
 
 
 func test_equality() -> void:
-	var rule := ManipulationRule.create(AlgebraicEquality.from_string("a=b"),
+	var rule := AlgebraicRule.create(AlgebraicEquality.from_string("a=b"),
 			AlgebraicEquality.from_string("(a+r)=(b+r)"))
 	var equality := AlgebraicEquality.from_string("x=y")
 	var result := rule.apply(equality)
